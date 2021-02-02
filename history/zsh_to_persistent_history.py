74ab2ffecca83e34bcf10eeef2e966b94acbf5cb
Jim Lawton <jim.lawton@gmail.com>
2021-02-02 21:40:40 +0000

    Default output file to input file, so script safely overwrites common history file.

--- history/zsh_to_persistent_history.py
+++ history/zsh_to_persistent_history.py
@@ -55,7 +55,7 @@ def main():
         "--outfile",
         type=str,
         metavar="FILE",
-        default="~/.persistent_history.new",
+        default="~/.persistent_history",
         help="Output persistent history file",
     )
     parser.add_argument(
