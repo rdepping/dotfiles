2acac7a500e4ba1a201384830fd9e2a2bf1deb09
Jim Lawton <jim.lawton@gmail.com>
2021-02-03 18:24:29 +0000

    Add window title override.

---
+++ override_window_title.zsh
@@ -0,0 +1,5 @@
+# This overrides ZSH's default window title, to remover user@host.
+# In a new setup, symlink to this file from oh-my-zsh/custom/.
+
+ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
+ZSH_THEME_TERM_TITLE_IDLE="%~"
