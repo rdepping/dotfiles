6e6ad131235b8e78ebc5e3c106926e47701f6c75
Jim Lawton <jim.lawton@gmail.com>
2021-02-08 17:18:41 +0000

    Change bang line to bash. Add setup script for misc zsh addons.

---
+++ _setup_zsh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d oh-my-zsh ]; then
+    if [ !-e oh-my-zsh/custom/override_window_title.zsh ]; then
+        (cd oh-my-zsh/custom; ln -sf ../../override_window_title.zsh)
+    fi
+    if [ ! -e oh-my-zsh/custom/plugins/fast-syntax-highlighting ]; then
+        (cd oh-my-zsh/custom/plugins; git clone https://github.com/zdharma/fast-syntax-highlighting.git)
+    fi
+    echo "Done!"
+fi
