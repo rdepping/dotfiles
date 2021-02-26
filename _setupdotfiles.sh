9c2a87dd9940282c182d5972b683dc3554dab787
Jim Lawton <jim.lawton@gmail.com>
2021-02-26 13:35:28 +0000

    Cleanup
    - Prune old dot files from the list.
    - Silently overwrite existing symlinks, stop if the destination is a file.

--- _setupdotfiles.sh
+++ _setupdotfiles.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 SHELLSCONFIGDIR=~/dotfiles
 DOTFILESDIR=~/dotfiles
-DOTFILES=".autoenv .bash_logout .bash_profile .bashrc .Brewfile .colordiffrc .colorgccrc .conf .gemrc .git_identities .gitconfig .gitignore .inputrc .iterm2_shell_integration.zsh .netrc .p10k.zsh .profile .pythonrc .rvmrc .shellactivities .shellaliases .shellpaths .shellvars .tmux .tmux.conf .vimrc .vim .xxdiffrc .zlogout .zprofile .zshenv .zshrc"
+DOTFILES=".bash_logout .bash_profile .bashrc .Brewfile .colordiffrc .colorgccrc .gitconfig .gitignore .inputrc .iterm2_shell_integration.zsh .netrc .p10k.zsh .profile .pythonrc .rvmrc .shellactivities .shellaliases .shellpaths .shellvars .tmux.conf .vimrc .xxdiffrc .zlogout .zprofile .zshenv .zshrc"
 DROPDIRS="bin .pip"
 MOVE=false
-SAVEDIR=~/.old
+SAVEDIR=~/.old/_setupdotfiles
 
 function symlinkifne {
-    target=~/$1
-    echo "Working on: $target"
+	target=~/$1
+	echo "Working on: $target"
 	export dotless=`echo $1 | sed s/^\.//`
-	if [ -e $target ]; then
+	if [ ! -e $DOTFILESDIR/$dotless ]; then
+		# Check if platform-specific version exists.
+		osname=$(uname)
+		if [ "$osname" = "Darwin" ]; then
+			platform="macos"
+		elif [ "$osname" = "Linux" ]; then
+			platform="linux"
+		else
+			echo "ERROR: unsupported OS $osname!"
+			exit 1
+		fi
+		if [ -e $DOTFILESDIR/$dotless-$platform ]; then
+			export dotless="$dotless-$platform"
+		else
+			echo "ERROR: file $DOTFILESDIR/$dotless does not exist, and has no platform-specific variant!"
+			exit 1
+		fi
+	fi
+	if [ ! -L $target ]; then
+		# If it's not a symlink, tread carefully.
 		echo "  WARNING: $target already exists!"
 		if [ "$MOVE" = "true" ]; then
 			echo "  Moving $target to $SAVEDIR/"
@@ -22,8 +41,10 @@ function symlinkifne {
 			echo "  Skipping $1."  
 		fi
 	else
-		echo "  Symlinking $DOTFILESDIR/$dotless to $1"
-		ln -s $DOTFILESDIR/$dotless $1
+		# Safe to just delete if it is a symlink (or if it doesn't exist).
+		rm -f $target
+		echo "  Symlinking $DOTFILESDIR/$dotless to $target"
+		ln -s $DOTFILESDIR/$dotless $target
 	fi
 }
 
@@ -34,11 +55,11 @@ pushd ~
 
 if [ "$MOVE" = "true" -a -d $SAVEDIR ]; then
 	echo "$SAVEDIR already exists! Please clean up and try again."
-	echo "This is usesd to save old versions of your configuration files."
+	echo "This is used to save old versions of your configuration files."
 	exit 1
 fi
 
-mkdir $SAVEDIR
+mkdir -p $SAVEDIR
 
 if [ ! -d dotfiles ]; then 
 	echo "The dotfiles dir does not exist in your home directory!"
